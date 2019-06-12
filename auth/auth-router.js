79a807247a11bb509d64efc2eb93943021d55499
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-12 13:17:46 -0700

    Login route works

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -8,7 +8,7 @@ const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model.js'); 
 
 // Requiring secrets 
-// const secrets = require('../config/secrets.js')
+const secrets = require('../config/secrets.js')
 
 // Requiring jswonwebtoken, after npm install 
 const jwt = require('jsonwebtoken');
@@ -31,5 +31,47 @@ router.post('/register', (req, res) => {
         res.status(500).json(error);
     });
 }); 
+
+// Logging a user in (C in CRUD)
+router.post('/login', (req, res) => {
+    let {username, password } = req.body; 
+
+    Users.findBy({ username })
+    .first()
+    .then(user => {
+        if(user && bcrypt.compareSync(password, user.password)) {
+            // Using tokens, we need to manually generate and send token as part of response 
+            // We'll write the function below 
+            const token = generateToken(user); 
+
+            res.status(200).json({
+                // After token generated, we have to add to response
+                message: `Welcome ${user.username}!`, token
+            }); 
+        } else {
+            res.status(401).json({ message: 'Invalid credentials.'}); 
+        } 
+    })
+    .catch(error => {
+        res.status(500).json(error); 
+    });
+});
+
+// Writing the function that generates our token 
+function generateToken(user) {
+    // Defining payload, containing claims (info) for token 
+    const payload = {
+        subject: user.id, 
+        username: user.username, 
+        department: user.department
+    }
+
+    const options = {
+        expiresIn: '1d'
+    }
+
+    // Grabs the jwtSecret to verify token 
+    return jwt.sign(payload, secrets.jwtSecret, options); 
+}
 
 module.exports = router; 
