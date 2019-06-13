6330c5c7a9c0fc749b215df5fe8ad3ec0f8bca22
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-13 15:11:16 -0700

    Added logout route

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,45 @@
 
 import React from 'react';
 // Now that I imported BrowserRouter in index.js, I can add Routes and NavLink in App.js 
-import { Route, NavLink } from 'react-router-dom'; 
+// Importing withRouter so we can redirect to login upon logout 
+import { Route, NavLink, withRouter } from 'react-router-dom'; 
 
 // Importing the components that will be returned in Routes 
 import SignIn from './SignIn'; 
 import SignUp from './SignUp'; 
 import Users from './Users'; 
 
-function App() {
-  return (
-    <>
-      <h1>Welcome!</h1>
-      <ul>
-        <li><NavLink to="/signin">Sign In</NavLink></li>
-        <li><NavLink to="/signup">Sign Up</NavLink></li>
-        <li><NavLink to="/users">Users</NavLink></li>
-      </ul>
-      
-      <div>
-        <Route path="/signin" component={SignIn} /> 
-        <Route path="/signup" component={SignUp} /> 
-        <Route path="/users" component={Users} /> 
-      </div>
+class App extends React.Component {
 
-    </> 
-  );
+  //Logout function, passed down to logout button 
+  logout = () => {
+    // Destroys the token 
+   localStorage.removeItem('token');  
+
+   // Redirects to login 
+   this.props.history.push('/signin'); 
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Welcome!</h1>
+        <ul>
+          <li><NavLink to="/signin">Sign In</NavLink></li>
+          <li><NavLink to="/signup">Sign Up</NavLink></li>
+          <li><NavLink to="/users">Users</NavLink></li>
+          <li><button onClick={this.logout}>Logout</button></li>
+        </ul>
+        
+        <div>
+          <Route path="/signin" component={SignIn} /> 
+          <Route path="/signup" component={SignUp} /> 
+          <Route path="/users" component={Users} /> 
+        </div>
+      </> 
+    ); 
+  }
 }
 
-export default App;
+
+export default withRouter(App);
