7ea53b9b9d3e6aefce057e5ac2dfe80412ca9849
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-13 14:44:53 -0700

    Users list displays

--- client/src/Users.js
+++ client/src/Users.js
@@ -1,14 +1,42 @@
 // We need a Users route as well 
 
 import React from 'react'; 
+// importing axios api helper
+import api from './helpers/api'; 
 
 class Users extends React.Component {
+    state = {
+        users: [], 
+    }
+
+    // Making a request to see list of users
+    async componentDidMount() {
+        try {
+            const result = await api.get('/users');
+            
+            // Setting my state to the users retrieved from the backend
+            this.setState(() => (
+                {
+                    users: result.data.users
+                }));
+            
+        } catch (err) {
+            console.error(err); 
+        }
+    }
+
     render() {
         return(
             <> 
                 <h3> Users </h3>
+
+                <ul>
+                    {this.state.users.map(user => (
+                        <li key={user.id}>{user.username}</li>
+                    ))}
+                </ul>
             </>
-        )
+        );
     }
 }
 
--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -4,6 +4,10 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'http://localhost:5500/api',
+    // Providing the server with a token, so that succesful logins can see users
+    headers: {
+        authorization: localStorage.getItem('token'),
+    },
 })
 
 export default instance; 
