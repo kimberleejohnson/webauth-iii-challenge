4cf4e95e21fe7be9e175142402174279d9e59cf4
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-13 12:55:00 -0700

    Added Users Route

---
+++ client/src/Users.js
@@ -0,0 +1,15 @@
+// We need a Users route as well 
+
+import React from 'react'; 
+
+class Users extends React.Component {
+    render() {
+        return(
+            <> 
+                <h3> Users </h3>
+            </>
+        )
+    }
+}
+
+export default Users; 
