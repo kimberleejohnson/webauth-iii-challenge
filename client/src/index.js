05e9456dbd8f326e0c4b3cbc2cdd69cf4117bd20
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-13 12:58:45 -0700

    Imported BrowserRouter

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// I need to turn all the empty component files I created into routes, so I import BrowserRouter
+// I'm not reassigning as Router because I'm only using it once 
+// I'll then wrap it around App 
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter>
+    <App />
+</BrowserRouter>, 
+document.getElementById('root'));
 
 
