6ebd617b739fb0749ac3b770b0d841d1d0bd18b4
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-13 13:15:02 -0700

    Built basic SignIn form console logging values

--- client/src/SignIn.js
+++ client/src/SignIn.js
@@ -3,10 +3,34 @@
 import React from 'react'; 
 
 class SignIn extends React.Component {
+    state = {
+        username: '',
+        password: '',
+    }
+
+    // Defining my change handler function 
+    handleSubmit = e => {
+        e.preventDefault(); 
+        console.log(this.state); 
+    }
+
+    handleChanges = e => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
+    }
+
     render() {
         return(
             <> 
                 <h3> Login </h3>
+                <form onSubmit={this.handleSubmit}> 
+                    <input type="text" name="username" placeholder="username" onChange={this.handleChanges} value={this.state.username} />
+
+                    <input type="password" name="password" placeholder="password" onChange={this.handleChanges} value={this.state.password} /> 
+
+                    <button type="submit">Sign In</button>
+                </form>
             </>
         )
     }
